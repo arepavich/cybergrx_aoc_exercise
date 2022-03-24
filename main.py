2595eca68565e8fc85fc177001c89d0235a0202e
Alex Repavich <merry.iron1816@fastmail.com>
2022-03-24 14:07:36 -0500

    Added basic validation

---
+++ main.py
@@ -0,0 +1,31 @@
+
+opening_brackets = "([{<"
+closing_brackets = ")]}>"
+
+def check_navdata(nav: str) -> bool:
+    """Validates a navigation subsystem's data
+
+    Checks navigation data for corrupted chunks and returns a boolean indicating whether the nav data is valid or not.
+    """
+    nav_stack = []
+
+    for char in nav:
+        if char in opening_brackets:
+            nav_stack.append(char)
+        if char in closing_brackets:
+            chunk_opener = nav_stack.pop()
+            # TODO(arepavich): Investigate pairing chunk openers/closers to allow concise checking of characters
+            if char == ")" and chunk_opener != "(":
+                return True
+            elif char == "]" and chunk_opener != "[":
+                return False
+            elif char == "}" and chunk_opener != "{":
+                return False
+            elif char == ">" and chunk_opener != "<":
+                return False
+
+    return True
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_main.py
@@ -0,0 +1,10 @@
+import pytest
+
+import main
+
+
+class TestCheckNavdata:
+    @pytest.mark.parametrize("navdata,expected", (("()", True), ("[]", True), ("(]", False), ("(()[])", True),))
+    def test_check_navdata_validates_correctly(self, navdata, expected):
+        result = main.check_navdata(navdata)
+        assert result is expected
